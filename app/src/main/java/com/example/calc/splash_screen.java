9b2cd7433429674d959a7ae5e30a943187fa8562
Arjun Adhikar <reasonworld11.com>
2020-08-09 17:56:30 +0545

    material_design fix

--- app/src/main/java/com/example/calc/splash_screen.java
+++ app/src/main/java/com/example/calc/splash_screen.java
@@ -16,12 +16,14 @@ public class splash_screen extends AppCompatActivity{
             EasySplashScreen config = new EasySplashScreen(splash_screen.this)
                     .withFullScreen()
                     .withTargetActivity(MainActivity.class)
-                    .withSplashTimeOut(3000)
-                    .withBackgroundResource(android.R.color.holo_red_light)
-                    .withHeaderText("Calculator")
+                    .withSplashTimeOut(3500)
+                    .withBackgroundResource(android.R.color.background_light)
+                    .withFooterText("Calculator")
                     .withBeforeLogoText("Learn and Share")
                     .withLogo(R.drawable.ic_baseline_collections_bookmark_24)
                     .withAfterLogoText("This is Just A Calculation App");
+            config.getFooterTextView().setTextSize(20);
+            config.getBeforeLogoTextView().setTextSize(19);
 
             //finally create the view
             View easySplashScreenView = config.create();
